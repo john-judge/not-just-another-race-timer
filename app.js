de147ea6eff63144f5053613cc3982cb645dc307
john-judge <jjudge@uchicago.edu>
2020-07-13 08:33:25 -0500

    connect to db

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 var http = require('http');
+ql      = require('mysql');
+
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    database : 'jmjudge2_njart_db',
+    user     : 'jmjudge2_user',
+    password : '^(nGbTxd{1Rp',
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.error('Error connecting: ' + err.stack);
+        return;
+    }
+
+    console.log('Connected as id ' + connection.threadId);
+});
+
 var server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    var message = 'It works!\n',
+    var message = 'Created server!\n',
         version = 'NodeJS ' + process.versions.node + '\n',
         response = [message, version].join('\n');
     res.end(response);
 });
 server.listen();
+connection.end();
