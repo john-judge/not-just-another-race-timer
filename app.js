2fd808e88e9d2cc10d31c2628181a684c39525df
John Judge <jmjudge2@illinois.edu>
2020-07-13 15:47:48 -0500

    html get and post

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let express = require("express");
 let mysql = require('mysql');
+let fs = require('fs');
 let port = 50000;
 
 const app = express();
@@ -12,9 +13,10 @@ let pool = mysql.createPool({
     password : '^(nGbTxd{1Rp',
 });
 
+//app.use(express.bodyParser());
 
-
-app.get("/not-just-another-race-timer",(req,resp) => {
+app.get("/not-just-another-race-timer",function  (req,resp) {
+    
     pool.getConnection((err, connection) => {
         if(err) throw err;
         console.log('connected as id ' + connection.threadId);
@@ -22,7 +24,22 @@ app.get("/not-just-another-race-timer",(req,resp) => {
             // call back function
             connection.release(); // return the connection to pool
             if(err) throw err;
-            console.log(rows[0].userID);
+            console.log(__dirname);
+            resp.sendfile(__dirname+"/home.html");
+        });
+    });
+	
+});
+
+
+app.post("/not-just-another-race-timer",(req,resp) => {
+    pool.getConnection((err, connection) => {
+        if(err) throw err;
+        console.log('connected as id ' + connection.threadId);
+        connection.query('SELECT * FROM Users', (err, rows) => {
+            // call back function
+            connection.release(); // return the connection to pool
+            if(err) throw err;
             resp.json(rows);
         });
     });
